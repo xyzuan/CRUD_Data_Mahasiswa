8ed1d04a259c5c0148c22b17f17d2c13ca9026a1
xyzuan <xyzuannihboss@gmail.com>
2021-12-25 18:30:15 +0700

    Prak6: Redesign some interface

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 	University of Muhammadiyah Malang
 */
 
+char rev[99] = "rev2.0_demo";
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -62,11 +64,14 @@ int main(){
     menu:
     system("cls");
     time_t localtime; time(&localtime);
-    printf("\n Sistem Pendataan Akun I-Lab Infotech");
-    printf("\n Database Time : %s\n", ctime(&localtime));
-    printf(" === Pilihan Menu ===\n\n");
+    printf(  " ====================================================");
+    printf("\n\n  Sistem Pendataan Akun I-Lab Infotech \n");
+    printf(  "\n  Software revision\t: %s\n", rev);
+    printf(    "  Database Time\t\t: %s\n", ctime(&localtime));
+    printf(  " ====================================================");
+    printf("\n\n === Pilihan Menu ===\n\n");
     printf(" 1. Create Data.\n 2. Show Data.\n 3. Update Data.\n 4. Delete Data.\n 5. Search Data.\n 6. Exit.");
-    printf("\n\n Masukkan Pilihan : "); scanf("%d" ,&pil_menu); 
+    printf("\n\n Masukkan Pilihan : "); scanf("%d" ,&pil_menu);
     switch (pil_menu){
     case 1:
         system("cls");
@@ -90,34 +95,34 @@ int main(){
                     if(sc_space == false){
                         printf("\n Input Kelas Pemrograman Dasar\t\t: "); fflush(stdin); scanf("%[^\n]" , db_kelas); uppercase(db_kelas);
                         fprintf(db_main,"%s|%s|%s\n", db_nim, db_nama, db_kelas);  
-                        printf("\n --------------------\n");
+                        printf("\n --------------------------------------------------------\n");
                         printf("\n Data Berhasil di Tambah.\n");
-                        printf("\n --------------------\n");
+                        printf("\n --------------------------------------------------------\n");
                     } else {
-                        printf("\n --------------------\n");
+                        printf("\n --------------------------------------------------------\n");
                         printf("\n Inputan Nama Tidak diperbolehkan\n");
                         printf(  " menggunakan spasi.\n");
-                        printf("\n --------------------\n");
+                        printf("\n --------------------------------------------------------\n");
                     }
                 } else {
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                     printf("\n Inputan NIM Terdeteksi Duplikat.\n");
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                 }
             } else {
-                printf("\n --------------------\n");
+                printf("\n --------------------------------------------------------\n");
                 printf("\n Inputan NIM tidak diperbolehkan\n");
                 printf(  " menggunakan simbol.\n");
-                printf("\n --------------------\n");
+                printf("\n --------------------------------------------------------\n");
             }
         } else {
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
             printf("\n Inputan NIM harus lengkap\n");
             printf(  " terdiri dari 17 karakter.\n");
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
         }
         fclose(db_main); fclose(db_main_r);
-        printf(" Tekan enter untuk kembali ke menu...");
+        printf("\n Tekan enter untuk kembali ke menu...");
         getch(); goto menu; break;
 
     case 2:
@@ -135,9 +140,9 @@ int main(){
         }
 
         if(available == false){
-            printf(" --------------------\n");
+            printf(" --------------------------------------------------------\n");
             printf("\n Data Tidak Tersedia.\n");
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
         }
 
         fclose(db_main_r);
@@ -160,16 +165,16 @@ int main(){
                 if(sc_space == false){
                     printf("\n Kelas Baru\t\t\t: "); fflush(stdin); scanf("%[^\n]", db_kelas); uppercase(db_kelas);
                     fprintf(db_main_w,"%s|%s|%s\n", db_nim, db_nama, db_kelas);
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                     printf("\n Data Berhasil di Edit.\n");
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                 } else {
                     system("cls");
                     printf(" === Update Data ===\n");
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                     printf("\n Inputan Nama Tidak diperbolehkan\n");
                     printf(  " menggunakan spasi.\n");
-                    printf("\n --------------------\n");
+                    printf("\n --------------------------------------------------------\n");
                     goto update_nama;
                 }
                 
@@ -178,9 +183,9 @@ int main(){
             }
         }
         if(available == false){
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
             printf("\n Data Tidak Tersedia.\n");
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
         }
         fclose(db_main_r); fclose(db_main_w);
         remove("mhs.db"); rename("mhs_temp.db", "mhs.db");
@@ -198,16 +203,16 @@ int main(){
                 fprintf(db_main_w,"%s|%s|%s\n", db_nim, db_nama, db_kelas);
             } else if (strcmp(db_nim, nim) != 1){
                 available = true;
-                printf("\n --------------------\n");
+                printf("\n --------------------------------------------------------\n");
                 printf("\n Data Berhasil di Hapus.\n");
-                printf("\n --------------------\n");
+                printf("\n --------------------------------------------------------\n");
             }
         }
 
         if(available == false){
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
             printf("\n Data Tidak Tersedia.\n");
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
         }
 
         fclose(db_main_r); fclose(db_main_w);
@@ -230,9 +235,9 @@ int main(){
             }
         }
         if(available == false){
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------\n");
             printf("\n Data Tidak di Temukan.\n");
-            printf("\n --------------------\n");
+            printf("\n --------------------------------------------------------");
         }
         fclose(db_main_r);
         printf("\n\n Tekan enter untuk kembali ke menu...");
